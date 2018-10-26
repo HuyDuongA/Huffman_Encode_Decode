07153a41d97d6fda11a548f92e55929b6ad0d19c
Huy Duong <haduong@calpoly.edu>
2018-10-25 18:30:49 -0700

    header is actually fully working now lol

--- file.c
+++ file.c
@@ -54,17 +54,18 @@ unsigned int open_file(int option, const char *file_name){
 }
 
 void header_gen(const uint32_t *array, int fd_w){
-    const uint8_t SIZE = 255;
     uint32_t c_count = 0;
+    uint8_t ch = 0;
     for(int i = 0; i < ASCII_SIZE; ++i){
         if(array[i])
             c_count++;
     }
     write(fd_w, &c_count, FOUR_BYTE);
-    for(uint8_t i = 0; i <= SIZE; ++i){
+    for(int i = 0; i < ASCII_SIZE; ++i){
         if(array[i]){
-            write(fd_w, &i, ONE_BYTE);
-            write(fd_w, &array[i], FOUR_BYTE);
+            write(fd_w, &ch, ONE_BYTE);
+            write(fd_w, &array[ch], FOUR_BYTE);
         }
+        ++ch;
     }
 }
